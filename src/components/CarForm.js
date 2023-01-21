79ee84895b2404ec75067094389ae09a31281278
Pedro Martos <martosmoleropedro@gmail.com>
2023-01-21 21:36:04 +0100

    car form deploy

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,5 +1,27 @@
+import { useDispatch } from "react-redux";
+
 function CarForm() {
-    return <div>form of cars</div>;
+    const handleNameChange = (event) => {
+        
+    };
+
+    return (
+    <div className="car-form panel">
+        <h4 className="subtitle is-3">Add Car</h4>
+        <form>
+            <div className="field-group">
+                <div className="field">
+                    <label className="label">Name</label>
+                    <input 
+                        className="input is-expanded"
+                        value={name}
+                        onChange={handleNameChange}   
+                        />
+                </div>
+            </div>
+        </form>  
+    </div>
+    );
 }
 
 export default CarForm;
