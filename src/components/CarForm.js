296439b8f263e569b2723478f4ae43190c3e56fd
Pedro Martos <martosmoleropedro@gmail.com>
2023-01-22 19:05:29 +0100

    recieve cost

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,14 +1,22 @@
 import { useDispatch, useSelector } from "react-redux";
-import { changeName } from '../store';
+import { changeName, changeCost } from '../store';
 
 function CarForm() {
     const dispatch = useDispatch();
-    const name = useSelector((state) => {
-        return state.form.name;
+    const { name, cost } = useSelector((state) => {
+        return {
+            name: state.form.name,
+            cost: state.form.cost
+        };
     });
 
     const handleNameChange = (event) => {
         dispatch(changeName(event.target.value));
+    };
+
+    const handleCostChange = (event) => {
+        const carCost = parseInt(event.target.value) || 0;
+        dispatch(changeCost(carCost));
     };
 
     return (
@@ -16,12 +24,21 @@ function CarForm() {
         <h4 className="subtitle is-3">Add Car</h4>
         <form>
             <div className="field-group">
-                <div className="field">
+            <div className="field">
                     <label className="label">Name</label>
                     <input 
                         className="input is-expanded"
                         value={name}
                         onChange={handleNameChange}   
+                        />
+                </div>
+                <div className="field">
+                    <label className="label">Cost</label>
+                    <input 
+                        className="input is-expanded"
+                        value={cost || ''}
+                        onChange={handleCostChange}
+                        type='number'   
                         />
                 </div>
             </div>
