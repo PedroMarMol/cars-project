acab24a76916af63c21979442ca51659d0710775
Pedro Martos <martosmoleropedro@gmail.com>
2023-01-22 19:49:31 +0100

    set car list

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -5,9 +5,34 @@ function CarList() {
         return state.cars.data;
 
     });
+
+    const handleCarDelete = (car) => {
+
+    }
+
+    const renderedCars = cars.map((car) => {
+        return (
+            <div key={car.id} className="panel">
+                <p>
+                    {car.name} - ${car.cost}
+                </p>
+                <button 
+                    className="button is-danger"
+                    onClick={() => handleCarDelete(car)}
+                >
+                    Delete
+                </button>
+            </div>
+        )
+    })
     
     console.log(cars);
-    return <div>list of cars</div>;
+    return ( 
+        <div className="car-list">
+            {renderedCars}
+            <hr />
+        </div>
+    );
 }
 
 export default CarList;
